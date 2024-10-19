917864e3e22a26c91fc641f046326f13a20721d6
aishse <email.friendme@gmail.com>
2024-10-18 19:06:17 -0700

    installed tailwind and react material UI

--- calhacks11.0/src/App.js
+++ calhacks11.0/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+// import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex p-10 bg-slate-300">
+      <Button variant="contained"> this is a tester button</Button>
     </div>
   );
 }
