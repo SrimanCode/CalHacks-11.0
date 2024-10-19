ab76bc5645455ad93a5480140673e2051a6fb3be
aishse <email.friendme@gmail.com>
2024-10-18 20:48:48 -0700

    basic frontend

--- calhacks11.0/src/App.js
+++ calhacks11.0/src/App.js
@@ -1,10 +1,18 @@
-import "./App.css";
 // import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 function App() {
   return (
-    <div className="flex p-10 bg-slate-300">
-      <Button variant="contained"> this is a tester button</Button>
+    <div className="flex h-screen flex-col items-center p-10 bg-slate-100">
+      <h1 className="text-3xl text-blue-800 font-bold ">
+        Pitch Analyzer Application
+      </h1>
+
+      <div className="mt-5 grid grid-cols-4 gap-4">
+        <Button variant="outlined">Button 1</Button>
+        <Button variant="outlined">Button 2</Button>
+        <Button variant="outlined">Button 3</Button>
+        <Button variant="outlined">Button 4</Button>
+      </div>
     </div>
   );
 }
