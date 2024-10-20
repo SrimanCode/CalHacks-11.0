3ab80e4b3f5293022c0144cef7a5c30e5e7c6cb8
aishse <email.friendme@gmail.com>
2024-10-19 17:00:46 -0700

    Update ModeToggle.js

--- calhacks11.0/src/Components/ModeToggle.js
+++ calhacks11.0/src/Components/ModeToggle.js
@@ -1,22 +1,21 @@
-import React, {useState} from "react";
-import Switch from '@mui/material/Switch';
+import React, { useState } from "react";
+import Switch from "@mui/material/Switch";
 import { FormControlLabel, FormGroup } from "@mui/material";
 
+export default function SwitchLabels({ onModeChange }) {
+  const [isMotivMode, setIsMotivMode] = useState(false);
 
-export default function SwitchLabels({onModeChange}){
-    const[isMotivMode, setIsMotivMode] = useState(false);
+  const handleChange = (event) => {
+    setIsMotivMode(event.target.checked);
+    onModeChange(event.target.checked);
+  };
 
-    const handleChange = (event) => {
-        setIsMotivMode(event.target.checked);
-        onModeChange(event.target.checked);
-    };
-
-    return(
-        <FormGroup className = "absolute top-2 right-2">
-            <FormControlLabel 
-                control={<Switch checked = {isMotivMode} onChange={handleChange}/>} 
-                label="Extra Motivation Mode"
-            />
-        </FormGroup>
-    );
+  return (
+    <FormGroup className="top-2 right-2">
+      <FormControlLabel
+        control={<Switch checked={isMotivMode} onChange={handleChange} />}
+        label="Extra Motivation Mode"
+      />
+    </FormGroup>
+  );
 }
